2f3d1ae018992badf031b28f5f71a2171a89c15a
Sebastian L Moran <sbssing7@gmail.com>
2023-10-05 21:14:10 -0500

    Demostraćión de producción y limpieza de código

--- src/componets/GifGrid.jsx
+++ src/componets/GifGrid.jsx
@@ -1,18 +1,16 @@
-import { useState } from "react";
+import { useEffect} from "react";
 import { getGifs } from "../helpers/getGifs";
 
 export const GifGrid = ({categoria}) => {
 
-    const [counter, setcounter] = useState(10);
+    useEffect(()=>{
+        getGifs(categoria);
+    }, [ ])
 
-    getGifs(categoria);
 
   return (
     <>
         <h3>{categoria}</h3>
-        <h5>{ counter}</h5>
-        <button onClick={() => setcounter(counter + 1)}>+1</button>
-
     </>
   )
 }
